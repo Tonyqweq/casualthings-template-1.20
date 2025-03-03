fbcb6431fc2ece1ea826864319c9f561345b87de
Tonyqwe <2419231380@qq.com>
2025-03-03 10:53:03 +0800

    添加新模块和功能，优化代码结构

---
+++ src/main/java/com/bessson/casualthings/item/ModToolMaterial.java
@@ -0,0 +1,62 @@
+package com.bessson.casualthings.item;
+
+import com.bessson.casualthings.item.ModItems;
+import net.fabricmc.yarn.constants.MiningLevels;
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.tag.ItemTags;
+
+import java.util.function.Supplier;
+
+public enum ModToolMaterial implements ToolMaterial{
+
+    CHICKEN_JEWEL(MiningLevels.NETHERITE,160,10.0f,2.5f,20,()->Ingredient.ofItems((ModItems.CHICKEN_JEWEL)));
+
+    private final int miningLevel;
+    private final int itemDurability;
+    private final float miningSpeed;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Supplier<Ingredient> repairIngredient;
+
+    ModToolMaterial(int miningLevel, int itemDurability, float miningSpeed, float attackDamage, int enchantability, Supplier<Ingredient> repairIngredient) {
+        this.miningLevel = miningLevel;
+        this.itemDurability = itemDurability;
+        this.miningSpeed = miningSpeed;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairIngredient = repairIngredient;
+    }
+
+
+    @Override
+    public int getDurability() {
+        return this.itemDurability;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return this.miningSpeed;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return this.miningLevel;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+}
